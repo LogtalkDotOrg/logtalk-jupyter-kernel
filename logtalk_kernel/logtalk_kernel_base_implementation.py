b48db94d24e580fe92face7f0b566cdfe6004789
Paulo Moura <pmoura@logtalk.org>
2025-03-02 08:47:29 +0000

    Update the `%%data` cell magic to also support plot legends

--- logtalk_kernel/logtalk_kernel_base_implementation.py
+++ logtalk_kernel/logtalk_kernel_base_implementation.py
@@ -860,6 +860,10 @@ class LogtalkKernelBaseImplementation:
             show_data_dict.pop("grid", None)
             plt.grid(**data_grid)
 
+        if "legend" in show_data_dict:
+            data_legend = show_data_dict["legend"]
+            show_data_dict.pop("legend", None)
+
         if data_type == "bar":
             plt.bar(**show_data_dict)
         elif data_type == "barh":
@@ -936,6 +940,9 @@ class LogtalkKernelBaseImplementation:
             plt.hist2d(data_x, data_y, **show_data_dict)
         elif data_type == "step":
             plt.step(**show_data_dict)
+
+        if 'data_legend' in locals():
+            plt.legend(**data_legend)
 
         fig = io.StringIO()
         plt.savefig(fig, format="svg")
